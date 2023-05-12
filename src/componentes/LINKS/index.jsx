c4e81b759f735ae609e3f33eee6a3e5b16c92460
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-05-12 17:40:44 -0300

    resolve mostrar vários menus de uma vez

--- src/componentes/LINKS/index.jsx
+++ src/componentes/LINKS/index.jsx
@@ -1,263 +1,207 @@
-
-
-
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import HomeIcon from '@mui/icons-material/Home';
-import LayersIcon from '@mui/icons-material/Layers';
-import BookIcon from '@mui/icons-material/Book';
-import KitchenIcon from '@mui/icons-material/Kitchen';
-import BeenhereIcon from '@mui/icons-material/Beenhere';
-import PeopleIcon from '@mui/icons-material/People';
-import LoginIcon from '@mui/icons-material/Login';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
-import PlayCircleIcon from '@mui/icons-material/PlayCircle';
-import BookmarksIcon from '@mui/icons-material/Bookmarks';
-import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
-import InfoIcon from '@mui/icons-material/Info';
-import StarIcon from '@mui/icons-material/Star';
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp'
+import MenuIcon from '@mui/icons-material/Menu'
+import CloseIcon from '@mui/icons-material/Close'
+import HomeIcon from '@mui/icons-material/Home'
+import LayersIcon from '@mui/icons-material/Layers'
+import BookIcon from '@mui/icons-material/Book'
+import KitchenIcon from '@mui/icons-material/Kitchen'
+import BeenhereIcon from '@mui/icons-material/Beenhere'
+import PeopleIcon from '@mui/icons-material/People'
+import LoginIcon from '@mui/icons-material/Login'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu'
+import PlayCircleIcon from '@mui/icons-material/PlayCircle'
+import BookmarksIcon from '@mui/icons-material/Bookmarks'
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd'
+import InfoIcon from '@mui/icons-material/Info'
+import StarIcon from '@mui/icons-material/Star'
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail'
 import { Link } from 'react-router-dom'
-import { Box, Stack, Typography } from "@mui/material";
-import * as Tag from "./styles";
+import { Box, Stack, Typography } from '@mui/material'
+import * as Tag from './styles'
 import Logo from '../../images/logo/logo-menu.png'
 import './index.css'
-import { useState } from 'react';
+import { useState } from 'react'
 
 const links = [
     {
         icon: <HomeIcon />,
         name: 'Home',
         onClick: 'home',
-        elements: {
-            HomePage1: 'Página principal',
-            HomePage2: 'Sub página',
-            HomePage3: 'Receitas favoritas',
-            HomePage4: 'Rascunho',
-        },
+        children: [
+            {
+                icon: '',
+                name: 'Página principal',
+                onClick: 'home',
+            },
+
+            { icon: '', name: 'Sub página', onClick: 'subPage' },
+            { icon: '', name: 'Receitas favoritas', onClick: 'favRecipes' },
+            { icon: '', name: 'Rascunho', onClick: 'draft' },
+        ],
     },
     {
         icon: <LayersIcon />,
         name: 'Pages',
         onClick: 'pages',
-        elementsPages: {
-            Page1: {
+        children: [
+            {
                 name: 'User Login',
-                icon: <LoginIcon />
+                icon: <LoginIcon />,
             },
-            Page2: { name: 'User Register', icon: <PersonAddIcon /> },
-            Page3: { name: 'Single Recipe', icon: <RestaurantMenuIcon /> },
-            Page4: { name: 'Single Video', icon: <PlayCircleIcon /> },
-            Page5: { name: 'Single Book', icon: <BookmarksIcon /> },
-            Page6: { name: 'Create Recipe', icon: <BookmarkAddIcon /> },
-            Page7: { name: 'About us', icon: <InfoIcon /> },
-            Page8: { name: 'Top Review', icon: <StarIcon /> },
-            Page9: { name: 'Contacts', icon: <AlternateEmailIcon /> },
-        }
+            { name: 'User Register', icon: <PersonAddIcon /> },
+            { name: 'Single Recipe', icon: <RestaurantMenuIcon /> },
+            { name: 'Single Video', icon: <PlayCircleIcon /> },
+            { name: 'Single Book', icon: <BookmarksIcon /> },
+            { name: 'Create Recipe', icon: <BookmarkAddIcon /> },
+            { name: 'About us', icon: <InfoIcon /> },
+            { name: 'Top Review', icon: <StarIcon /> },
+            { name: 'Contacts', icon: <AlternateEmailIcon /> },
+        ],
     },
     { icon: <KitchenIcon />, name: 'Recitas', onClick: 'recipes' },
     { icon: <BookIcon />, name: 'Blog', onClick: 'blog' },
     { icon: <PeopleIcon />, name: 'Comunidade', onClick: 'community' },
 ]
 export const Links_a = ({
-    name, onClick, icon, elements, elementsPages,
+    name,
+    handleClick,
+    icon,
+    children,
+    selectedLink,
 }) => {
     const [show, setShow] = useState(true)
-    const [home, setHome] = useState(false)
-    const [pages, setPages] = useState(false)
-    const showSubMenu = () => {
-        setShow(!show)
-        if (name === 'Home') {
-            setHome(!home)
-        } 
-        if (name === 'Pages') {
-            setPages(!pages)
-        }
-    }
+
+    const isSelected = selectedLink === name
+
     return (
-        <>
-            <Tag.ContainLinks onClick={showSubMenu} sx={!show ? {
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                cursor: 'pointer',
-                textTransform: "uppercase",
-                color: 'white',
-                textDecoration: 'none',
-                fontWeight: '700',
-                borderLeft: '10px solid gray',
-                '.iconLnk': {
-                    display: 'none'
-                },
-            } : {}}>
-                <Tag.Link
+        <Tag.ContainLinks
+            onClick={handleClick}
+            sx={
+                !show
+                    ? {
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          cursor: 'pointer',
+                          textTransform: 'uppercase',
+                          color: 'white',
+                          textDecoration: 'none',
+                          fontWeight: '700',
+                          borderLeft: '10px solid gray',
+                          '.iconLnk': {
+                              display: 'none',
+                          },
+                      }
+                    : {}
+            }
+        >
+            <Tag.Link>
+                <Tag.IconLink className="iconLnk">{icon}</Tag.IconLink>
+                <Tag.Aa>
+                    <Box>{name}</Box>
+                </Tag.Aa>
+                <Tag.ShowlinkIcon>
+                    {show ? (
+                        <ArrowDropDownIcon
+                            fontSize="large"
+                            sx={{
+                                color: 'text.secondary',
+                            }}
+                        />
+                    ) : (
+                        <ArrowDropUpIcon
+                            sx={{
+                                color: 'text.secondary',
+                            }}
+                            fontSize="large"
+                        />
+                    )}
+                </Tag.ShowlinkIcon>
+            </Tag.Link>
+
+            {isSelected && (
+                <Tag.SubMenus
+                    sx={{
+                        color: 'text.secondary',
+                        textTransform: 'lowercase',
+                    }}
                 >
-                    <Tag.IconLink className='iconLnk'>
-                        {icon}
-                    </Tag.IconLink>
-                    <Tag.Aa to={onClick}>
-                        <Box>
-                            {name}
-                        </Box>
-                    </Tag.Aa>
-                    <Tag.ShowlinkIcon >
-                        {show ? <ArrowDropDownIcon fontSize='large' sx={{
-                            color: 'text.secondary'
-                        }} /> : <ArrowDropUpIcon sx={{
-                            color: 'text.secondary'
-                        }} fontSize='large' />}
-                    </Tag.ShowlinkIcon>
-                </Tag.Link>
-                {home &&
-                    <Tag.SubMenus sx={{
-                        color: 'text.secondary',
-                        textTransform: 'lowercase'
-                    }}>
-                        <Tag.Branch>{elements?.HomePage1}</Tag.Branch>
-                        <Tag.Branch>{elements?.HomePage2}</Tag.Branch>
-                        <Tag.Branch>{elements?.HomePage3}</Tag.Branch>
-                        <Tag.Branch>{elements?.HomePage4}</Tag.Branch>
-                    </Tag.SubMenus>
-                }
-                {pages &&
-                    <Tag.SubMenus sx={{
-                        color: 'text.secondary',
-                        textTransform: 'lowercase'
-                    }}>
-
-                        <Tag.Branch>
-                            <Box>
-                                {elementsPages?.Page1?.icon}
-                            </Box>
-                            <Box>
-                                {elementsPages?.Page1?.name}
-                            </Box>
-                        </Tag.Branch>
-                        <Tag.Branch>
-                            <Box>
-                                {elementsPages?.Page2?.icon}
-                            </Box>
-                            <Box>
-                                {elementsPages?.Page2?.name}
-                            </Box>
-                        </Tag.Branch>
-                        <Tag.Branch>
-                            <Box>
-                                {elementsPages?.Page3?.icon}
-                            </Box>
-                            <Box>
-                                {elementsPages?.Page3?.name}
-                            </Box>
-                        </Tag.Branch>
-                        <Tag.Branch>
-                            <Box>
-                                {elementsPages?.Page4?.icon}
-                            </Box>
-                            <Box>
-                                {elementsPages?.Page4?.name}
-                            </Box>
-                        </Tag.Branch>
-                        <Tag.Branch>
-                            <Box>
-                                {elementsPages?.Page5?.icon}
-                            </Box>
-                            <Box>
-                                {elementsPages?.Page5?.name}
-                            </Box>
-                        </Tag.Branch>
-                        <Tag.Branch>
-                            <Box>
-                                {elementsPages?.Page6?.icon}
-                            </Box>
-                            <Box>
-                                {elementsPages?.Page6?.name}
-                            </Box>
-                        </Tag.Branch>
-                        <Tag.Branch>
-                            <Box>
-                                {elementsPages?.Page7?.icon}
-                            </Box>
-                            <Box>
-                                {elementsPages?.Page7?.name}
-                            </Box>
-                        </Tag.Branch>
-                        <Tag.Branch>
-                            <Box>
-                                {elementsPages?.Page8?.icon}
-                            </Box>
-                            <Box>
-                                {elementsPages?.Page8?.name}
-                            </Box>
-                        </Tag.Branch>
-                        <Tag.Branch>
-                            <Box>
-                                {elementsPages?.Page9?.icon}
-                            </Box>
-                            <Box>
-                                {elementsPages?.Page9?.name}
-                            </Box>
-                        </Tag.Branch>
-
-                    </Tag.SubMenus>
-                }
-
-            </Tag.ContainLinks>
-        </>
+                    {children &&
+                        children?.length > 0 &&
+                        children.map((child) => (
+                            <Tag.Branch>
+                                {child.icon} {child.name}
+                            </Tag.Branch>
+                        ))}
+                </Tag.SubMenus>
+            )}
+        </Tag.ContainLinks>
     )
 }
 
-
-
 export const Links = () => {
-    const ShowLinks = () => {
+    const ShowLinks = () => {}
 
-    }
-    const openMenu = () => {
+    const [selectedLink, setSelectedLink] = useState()
 
+    const handleSelectLink = (newLink) => {
+        console.log(newLink)
+        setSelectedLink(newLink)
     }
+
     return (
         <>
-            <Tag.Menu_links className='display-block'>
-                <Tag.Logo variant='h4'>
+            <Tag.Menu_links className="display-block">
+                <Tag.Logo variant="h4">
                     <img src={Logo} alt="" />
                 </Tag.Logo>
-                <Tag.ShowlinkIcon >
-                    <MenuIcon fontSize='large' onClick={ShowLinks} />
+                <Tag.ShowlinkIcon>
+                    <MenuIcon fontSize="large" />
                 </Tag.ShowlinkIcon>
 
                 <Tag.Links>
                     <Tag.LogoIconMobile
-                        direction='row'
+                        direction="row"
                         display="flex"
-                        justifyContent='space-between'
-                        alignItems='center'
+                        justifyContent="space-between"
+                        alignItems="center"
                         width="100%"
                     >
-                        <Tag.IconMenu sx={{
-                            color: 'text.secondary'
-                        }} variant='h4'>
+                        <Tag.IconMenu
+                            sx={{
+                                color: 'text.secondary',
+                            }}
+                            variant="h4"
+                        >
                             <img src={Logo} alt="" />
                         </Tag.IconMenu>
-                        <Tag.Div className='none display' sx={{
-                            color: 'text.secondary',
-                            cursor: 'pointer',
-                        }}>
-                            <CloseIcon className='closeMenu' onClick={ShowLinks} fontSize='large' />
+                        <Tag.Div
+                            className="none display"
+                            sx={{
+                                color: 'text.secondary',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            <CloseIcon
+                                className="closeMenu"
+                                onClick={ShowLinks}
+                                fontSize="large"
+                            />
                         </Tag.Div>
                     </Tag.LogoIconMobile>
-                    {
-                        links.map((li) => {
-                            return (
-
-                                <Links_a onClick={openMenu} key={li.name} {...li} />
-                            )
-                        })
-                    }
+                    {links.map((li) => {
+                        return (
+                            <Links_a
+                                key={li.name}
+                                {...li}
+                                handleClick={() => handleSelectLink(li.name)}
+                                selectedLink={selectedLink}
+                            />
+                        )
+                    })}
                 </Tag.Links>
-
             </Tag.Menu_links>
         </>
     )
