60957d33dabfb6eaf504de677fa9662e12c44336
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-05-31 18:06:55 -0300

    corrige import

--- src/componentes/LINKS/index.jsx
+++ src/componentes/LINKS/index.jsx
@@ -67,8 +67,9 @@ import Logo from '../../images/logo/logo-menu.png'
 import './index.css'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { AuthContext } from '../../App'
-import { useContext } from 'react';
+
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 const links = [
     {
@@ -83,7 +84,11 @@ const links = [
             },
 
             { icon: <LastPageIcon />, name: 'Sub página', onClick: 'subPage' },
-            { icon: <BookmarkIcon />, name: 'Receitas favoritas', onClick: 'favRecipes' },
+            {
+                icon: <BookmarkIcon />,
+                name: 'Receitas favoritas',
+                onClick: 'favRecipes',
+            },
             { icon: <EditNoteIcon />, name: 'Rascunho', onClick: 'draft' },
         ],
     },
@@ -95,13 +100,17 @@ const links = [
             {
                 name: 'User Login',
                 icon: <LoginIcon />,
-                link: '/signin'
+                link: '/signin',
             },
             { name: 'User Register', icon: <PersonAddIcon /> },
             { name: 'Single Recipe', icon: <RestaurantMenuIcon /> },
             { name: 'Single Video', icon: <PlayCircleIcon /> },
             { name: 'Single Book', icon: <BookmarksIcon /> },
-            { name: 'Create Recipe', icon: <BookmarkAddIcon />, link: '/createRecipes' },
+            {
+                name: 'Create Recipe',
+                icon: <BookmarkAddIcon />,
+                link: '/createRecipes',
+            },
             { name: 'About us', icon: <InfoIcon /> },
             { name: 'Top Review', icon: <StarIcon /> },
             { name: 'Contacts', icon: <AlternateEmailIcon /> },
@@ -119,26 +128,43 @@ const links = [
                 onClick: 'blog1',
             },
 
-            { icon: <MarkUnreadChatAltIcon />, name: 'Blog 2', onClick: 'blog2' },
-            { icon: <QuestionAnswerIcon />, name: 'Blog 3 (Single)', onClick: 'blog2' },
+            {
+                icon: <MarkUnreadChatAltIcon />,
+                name: 'Blog 2',
+                onClick: 'blog2',
+            },
+            {
+                icon: <QuestionAnswerIcon />,
+                name: 'Blog 3 (Single)',
+                onClick: 'blog2',
+            },
         ],
     },
     { icon: <PeopleIcon />, name: 'Comunidade', onClick: 'community' },
     {
-        icon: <SettingsIcon />, name: 'Advanced settings', onClick: 'adSettings',
+        icon: <SettingsIcon />,
+        name: 'Advanced settings',
+        onClick: 'adSettings',
         children: [
-
-            { icon: <TerminalIcon/>, name: 'Mode root', onClick: 'root' },
-            { icon: <AdminPanelSettingsIcon/>, name: 'Mode admin', onClick: 'admin' },
-            { icon: <TaxiAlertIcon/>, name: 'Red alert mode ', onClick: 'Red' },
+            { icon: <TerminalIcon />, name: 'Mode root', onClick: 'root' },
+            {
+                icon: <AdminPanelSettingsIcon />,
+                name: 'Mode admin',
+                onClick: 'admin',
+            },
+            {
+                icon: <TaxiAlertIcon />,
+                name: 'Red alert mode ',
+                onClick: 'Red',
+            },
             { icon: <LogoutIcon />, name: 'Log out', onClick: 'logout' },
             {
-                icon: <BlockIcon/>,
+                icon: <BlockIcon />,
                 name: 'Delete account',
                 onClick: 'deleaccont',
             },
         ],
-    }
+    },
 ]
 
 const Links_b = ({
@@ -150,15 +176,15 @@ const Links_b = ({
     children,
     selectedLink,
 }) => {
-    const { logout } = useContext(AuthContext);
+    const { logout } = useContext(AuthContext)
     const isSelected = selectedLink === name
     const handleLinkClick = (link) => {
         if (link.onClick === 'logout') {
-            logout(); // Chame a função logout aqui
+            logout() // Chame a função logout aqui
         } else {
             // Lógica para lidar com outros links
         }
-    };
+    }
     return (
         <>
             <Stack
@@ -174,12 +200,8 @@ const Links_b = ({
                 aria-expanded={isSelected ? 'true' : undefined}
                 onClick={handleClick}
             >
-                <Stack>
-                    {icon}
-                </Stack>
-                <Stack>
-                    {name}
-                </Stack>
+                <Stack>{icon}</Stack>
+                <Stack>{name}</Stack>
             </Stack>
             <Menu
                 id="basic-menu"
@@ -192,9 +214,14 @@ const Links_b = ({
             >
                 {children?.length > 0 &&
                     children.map((child, index) => (
-                        <MenuItem key={index} onClick={() => handleLinkClick(child)}>
+                        <MenuItem
+                            key={index}
+                            onClick={() => handleLinkClick(child)}
+                        >
                             <ListItemIcon>{child.icon}</ListItemIcon>
-                            <ListItemText><Link href={child.link}>{child.name}</Link></ListItemText>
+                            <ListItemText>
+                                <Link href={child.link}>{child.name}</Link>
+                            </ListItemText>
                         </MenuItem>
                     ))}
             </Menu>
@@ -208,14 +235,14 @@ export const Links_a = ({
     children,
     selectedLink,
 }) => {
-    const { logout } = useContext(AuthContext);
+    const { logout } = useContext(AuthContext)
     const handleLinkClick = (link) => {
         if (link.onClick === 'logout') {
-            logout(); // Chame a função logout aqui
+            logout() // Chame a função logout aqui
         } else {
             // Lógica para lidar com outros links
         }
-    };
+    }
     const isSelected = selectedLink === name
 
     return (
@@ -230,7 +257,11 @@ export const Links_a = ({
                     {children &&
                         children?.length > 0 &&
                         children.map((child) => (
-                            <ListItemButton key={child?.name} onClick={() => handleLinkClick(child)} sx={{ pl: 4, borderLeft: '20px solid white' }}>
+                            <ListItemButton
+                                key={child?.name}
+                                onClick={() => handleLinkClick(child)}
+                                sx={{ pl: 4, borderLeft: '20px solid white' }}
+                            >
                                 <ListItemIcon>{child.icon}</ListItemIcon>
                                 <Link href={child?.link}>
                                     <ListItemText primary={child.name} />
@@ -246,32 +277,31 @@ export const Links_a = ({
 }
 
 export const Links = () => {
-    const { logout } = useContext(AuthContext);
-    const [scrollHeight, setScrollHeight] = useState(0);
+    const { logout } = useContext(AuthContext)
+    const [scrollHeight, setScrollHeight] = useState(0)
     const [selectedLink, setSelectedLink] = useState()
     const [anchorEl, setAnchorEl] = useState(null)
     useEffect(() => {
         const handleScroll = () => {
-            const height = window.scrollY || 0;
-            setScrollHeight(height);
-        };
+            const height = window.scrollY || 0
+            setScrollHeight(height)
+        }
         if (typeof window !== 'undefined') {
-            window.addEventListener('scroll', handleScroll);
+            window.addEventListener('scroll', handleScroll)
 
             // Limpe o evento de scroll quando o componente for desmontado
             return () => {
-                window.removeEventListener('scroll', handleScroll);
-            };
+                window.removeEventListener('scroll', handleScroll)
+            }
         }
-    }, []);
+    }, [])
 
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth',
-        });
-    };
-
+        })
+    }
 
     const handleSelectLink = (event, newLink) => {
         if (selectedLink === newLink) {
@@ -279,7 +309,7 @@ export const Links = () => {
             return
         }
 
-        console.log(selectedLink, newLink.onClick);
+        console.log(selectedLink, newLink.onClick)
         setSelectedLink(newLink)
         setAnchorEl(event.target)
     }
@@ -346,9 +376,7 @@ export const Links = () => {
                 </Stack>
                 <Stack>
                     {!showLinks ? (
-                        <MenuIcon
-                            onClick={(e) => setShowLinks(!showLinks)}
-                        />
+                        <MenuIcon onClick={(e) => setShowLinks(!showLinks)} />
                     ) : (
                         <MenuOpenIcon
                             onClick={(e) => setShowLinks(!showLinks)}
@@ -376,9 +404,7 @@ export const Links = () => {
                             <img src={Logo} alt="" />
                             {!matchesMobileSmall && (
                                 <CloseIcon
-                                    onClick={(e) =>
-                                        setShowLinks(!showLinks)
-                                    }
+                                    onClick={(e) => setShowLinks(!showLinks)}
                                 />
                             )}
                         </Tag.ListSub>
@@ -399,6 +425,5 @@ export const Links = () => {
                 </Tag.MinhaLista>
             )}
         </Box>
-
     )
 }
