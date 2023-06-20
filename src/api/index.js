839c1d23ee1d9513c4c8635b4ba00fd19ff659eb
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-06-20 17:54:19 -0300

    rename function delete recipe

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import {
   addDoc,
   arrayUnion,
   collection,
+  deleteDoc,
   doc,
   getDoc,
   getDocs,
@@ -191,13 +192,12 @@ export const api = {
           const recipesRef = collection(db, 'recipes');
           const q = query(recipesRef, where('authorId', '==', authorId));
           const snapshot = await getDocs(q);
-
           const recipes = [];
           snapshot.forEach((doc) => {
             const recipe = doc.data();
+            recipe.id = doc.id; // Adicionar a propriedade "id" ao objeto da receita
             recipes.push(recipe);
           });
-
           return recipes;
         } catch (error) {
           console.error('Erro ao buscar as receitas:', error);
@@ -205,6 +205,18 @@ export const api = {
         }
       } else {
         return []; // Retorna uma lista vazia se o ID do autor não for fornecido
+      }
+    },
+    delete: async (recipeId) => {
+      if (recipeId) {
+        try {
+          await deleteDoc(doc(db, 'recipes', recipeId));
+          console.log('Receita deletada com sucesso');
+          // Faça algo após deletar a receita, se necessário
+        } catch (error) {
+          console.error('Erro ao deletar a receita:', error);
+          // Trate o erro adequadamente
+        }
       }
     }
   },
