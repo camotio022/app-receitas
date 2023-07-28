b8dd65e51e3ec1bb59a17268c9a4c5f9542043bf
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-07-28 13:57:10 -0300

    passando a prop type

--- src/componentes/textField/textField.jsx
+++ src/componentes/textField/textField.jsx
@@ -3,15 +3,17 @@ import { Grid, TextField } from "@mui/material"
 export const MyTextField = ({
     variant,
     fullWidth,
+    type,
     ...props
 }) => {
     return (
         <>
-            <Grid item xs={12} lg={6} sx={{mb: 1, width: '100%'}}>
+            <Grid item xs={12} lg={6} sx={{ mb: 1, width: '100%' }}>
                 <TextField
                     required
                     fullWidthfullWidth
                     fullWidth
+                    type={type}
                     variant={variant || 'filled'}
                     {...props}
                 />
