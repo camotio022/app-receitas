acaa40f00a8354230d766c1bd3ccb85587a7d1b7
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-07-28 13:55:25 -0300

    cria os componentes e sapara validação do cadastro e também remove os textfields

---
+++ src/auth/signUp/componentes/sections.js
@@ -0,0 +1,22 @@
+export const sections = [
+    {
+        type: 'string',
+        name: 'name',
+        label: 'Seu nome',
+    },
+    {
+        type: 'string',
+        name: 'lastName',
+        label: 'Last name',
+    },
+    {
+        type: 'email',
+        name: 'email',
+        label: 'Email',
+    },
+    {
+        type: 'password',
+        name: 'password',
+        label: 'Password',
+    }
+]
