0017a9b553a62c75388e28925fe6e4bcf6160cba
camotio02 <temotio.bernardo@snowmanlabs.com>
2023-07-28 23:17:57 -0300

    adiciona o link nos resultados

--- src/pages/MainLayout/search/componentes/recipesresults.jsx
+++ src/pages/MainLayout/search/componentes/recipesresults.jsx
@@ -4,34 +4,44 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-export const FolderList = ({
-    results,
-    searchInput,
-    key
-}) => {
+import { Link } from '@mui/material';
+export const FolderList = ({ results, searchInput }) => {
+    if (!results || !Array.isArray(results)) {
+
+        return null;
+    }
+
     return (
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }} >
-            {results?.map((item) => {
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            {results.map((item) => {
                 return (
-                    <ListItem key={key}>
-                        <ListItemAvatar>
-                            <Avatar src={item.recipeImage}>
-                                {item.recipeImage}
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary={item.recipeTitle && (
-                            <span
-                                dangerouslySetInnerHTML={{
-                                    __html: item.recipeTitle.replace(
-                                        new RegExp(searchInput, 'gi'),
-                                        (match) => `<strong class="results">${match}</strong>`
-                                    ),
-                                }}
-                            />
-                        )} secondary={item.creationDate} />
-                    </ListItem>
-                )
+                    <Link key={item.id} href={`/detailsRecipes/${item.id}`}>
+                        {/* Certifique-se de fornecer um valor para 'to' ou 'href' */}
+                        <List>
+                            <ListItem >
+                                <ListItemAvatar>
+                                    <Avatar src={item.recipeImage}>{item.recipeImage}</Avatar>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary={
+                                        item.recipeTitle && (
+                                            <span
+                                                dangerouslySetInnerHTML={{
+                                                    __html: item.recipeTitle.replace(
+                                                        new RegExp(searchInput, 'gi'),
+                                                        (match) => `<strong class="results">${match}</strong>`
+                                                    ),
+                                                }}
+                                            />
+                                        )
+                                    }
+                                    secondary={item.creationDate}
+                                />
+                            </ListItem>
+                        </List>
+                    </Link>
+                );
             })}
-        </List >
+        </List>
     );
-}
+};
