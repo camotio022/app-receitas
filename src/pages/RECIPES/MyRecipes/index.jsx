47dee46f20955724eca25e35a0a426da8dba710c
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-08-09 16:35:31 -0300

    import dos icons das tabs

--- src/pages/RECIPES/MyRecipes/index.jsx
+++ src/pages/RECIPES/MyRecipes/index.jsx
@@ -12,6 +12,8 @@ import { ProgressMyRecipes } from './componentes/progress'
 import { OptionsMyRecipes } from './componentes/options'
 import { CardMyRecipes } from './componentes/cardRecipes'
 import { api_myrecipes } from '../../../api/recipes/myrecipes'
+import { AppBarGlobal } from '../../../componentes/AppBar'
+import { Folder } from '@mui/icons-material'
 function TabPanel(props) {
   const { children, value, index, ...other } = props
   return (
@@ -98,12 +100,16 @@ export const MyRecipes = () => {
   const handleChange = (event, newValue) => {
     setValue(newValue)
   }
+  const tabs = [
+    { icon: Folder, text: "MINHAS RECEITAS" },
+  ];
   return (
 
     <>
-      <AppBarMyrecipes
+      <AppBarGlobal
         handleChange={handleChange}
         value={value}
+        tabs={tabs}
       />
       {isLoading ? (
         <>
