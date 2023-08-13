f9fc9b5b5d59fd9364184e989f36817808b58114
camotio02 <temotio.bernardo@snowmanlabs.com>
2023-08-12 21:51:04 -0300

    Criando comentários

--- src/pages/RECIPES/DetailsRecipes/index.jsx
+++ src/pages/RECIPES/DetailsRecipes/index.jsx
@@ -1,5 +1,7 @@
 import {
-    Card,
+    Avatar,
+    Button,
+    Card, Grid, List, ListItem, ListItemAvatar, ListItemText, Stack, TextField, Typography,
 } from '@mui/material'
 import { useParams } from 'react-router-dom'
 import * as Tag from './styles/index.js'
@@ -12,11 +14,26 @@ import { StepsDetailsRecipe } from './componentes/StepsDetailsRecipe/index.jsx'
 import { LoadingDetailsRecipe } from './componentes/loadingDetailsRecipe/index.jsx'
 import { api_recipes } from '../../../api/recipes/recipes.js'
 import { api_more } from '../../../api/index.js'
+// ... O restante do seu código ...
+
+const Comment = ({ author, content }) => (
+    <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+            <Avatar alt={author} src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+            primary={author}
+            secondary={content}
+        />
+    </ListItem>
+);
 export const DetailsRecipes = () => {
     const { id } = useParams()
     const [recipe, setrecipe] = useState(null)
     const [ingredientes, setingredientes] = useState([])
-    const [modopreparos, setmodopreparos] = useState([])
+    const [modopreparos, setmodopreparos] = useState(['o', 'o'])
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState('');
     useEffect(() => {
         const obterDetalhesrecipe = async () => {
             const doc = await api_recipes.recipe.get(id)
@@ -34,6 +51,14 @@ export const DetailsRecipes = () => {
         }
         obterDetalhesrecipe()
     }, [id])
+    const handleSubmit = () => {
+
+        if (newComment) {
+            setComments([...comments, { author: 'User', content: newComment, replies: [] }]);
+            setNewComment('');
+        }
+    };
+
     if (!recipe) {
         return (
             <>
@@ -43,8 +68,6 @@ export const DetailsRecipes = () => {
     }
     return (
         <>
-
-
             <Tag.Container>
                 <Tag.CardMediaContain>
                     <CardMidiaDetailRecipe
@@ -65,9 +88,36 @@ export const DetailsRecipes = () => {
                         }}
                     >
                     </Card>
-                </Tag.CardMediaContain>
-            </Tag.Container>
+                    <Typography>Comments</Typography>
+                    <List style={{ width: '90%', margin: 'auto' }}>
+                        {comments.map((comment, index) => (
+                            <Comment
+                                key={index}
+                                author={comment.author}
+                                content={comment.content}
+                            />
+                        ))}
+                    </List>
+                    <Grid container alignItems="center" spacing={2} sx={{ width: '90%' }}>
+                        <Grid item xs={12} md={9}>
+                            <TextField
+                                label="Add a comment"
+                                variant="outlined"
+                                fullWidth
+                                value={newComment}
+                                onChange={(e) => setNewComment(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}
+                            md={3} mb={3} >
+                            <Button fullWidth onClick={handleSubmit} variant="contained" color="primary" >
+                                Add Comment
+                            </Button>
+                        </Grid>
+                    </Grid>
 
+                </Tag.CardMediaContain>
+            </Tag.Container >
 
         </>
     )
