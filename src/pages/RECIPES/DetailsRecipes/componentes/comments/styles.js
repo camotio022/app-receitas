05e241c7536ed7f20650b79648a8a35b78d27fd3
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-08-22 17:22:31 -0300

    criando os estilos da página de componentes

---
+++ src/pages/RECIPES/DetailsRecipes/componentes/comments/styles.js
@@ -0,0 +1,17 @@
+import { Stack, styled } from "@mui/material";
+export const ContainerComments = styled(Stack)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+}))
+export const StepsContain = styled(Stack)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    width: '100%',
+    backgroundColor: '#f5f5f5',
+    padding: '8px 8px 8px 25px',
+    borderRadius: '4px',
+    marginBottom: '8px',
+    borderLeft: '1px solid gray',
+}))
