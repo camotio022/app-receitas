d0e44ba6bc0768760caf99f9b12cf67d1447a66e
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-08-24 16:38:39 -0300

    pegando a imagem padrão para as receitas sem as imagens

--- src/componentes/CARDRECIPE/index.jsx
+++ src/componentes/CARDRECIPE/index.jsx
@@ -19,38 +19,45 @@ export const CardRecipe = ({
 }) => {
     const matches = useMediaQuery('(min-width:700px)')
     const { user } = useContext(AuthContext)
-    const comment =()=> {
+    const comment = () => {
         alert('Deseja comentar essa receita?')
     }
     return (
-        <Tag.Card key={id} onDoubleClick={()=>comment(user.uid)}>
+        <Tag.Card key={id} onDoubleClick={() => comment(user.uid)}>
             <Stack width={'100%'}>
                 <Tooltip
                     sx={{ cursor: 'pointer' }}
-                    title={`Ir para os detalhes  ${recipeTitleHoverHeader}`}
+                    title={`Dê dois clicks para comentar está receita  ${recipeTitle}`}
                     followCursor
                 >
                     <img
+                        style={{ height: '10rem' }}
                         className="img"
-                        src={recipeImage}
-                        alt=""
+                        src={recipeImage? recipeImage: 'https://saturdaykitchenrecipes.com/wp-content/uploads/2020/04/default-recipe-image.gif'}
+                        alt={`receita de ${recipeTitle}`}
                     />
                 </Tooltip>
                 <Stack padding={2} spacing={2}>
-                    <Typography color={'gray'} variant="h6" sx={{
-                        overflow: 'hidden',
-                        whiteSpace: 'nowrap',
-                        textOverflow: 'ellipsis',
-                        fontSize: '100%'
-                    }}>
-                        <Link
-                            href={`/detailsRecipes/${id}`}
-                            color="inherit"
-                            underline="hover"
-                        >
-                            {recipeTitle}
-                        </Link>
-                    </Typography>
+                    <Tooltip
+                        sx={{ cursor: 'pointer' }}
+                        title={`Ir para os detalhes  ${recipeTitleHoverHeader}`}
+                        followCursor
+                    >
+                        <Typography color={'gray'} variant="h6" sx={{
+                            overflow: 'hidden',
+                            whiteSpace: 'nowrap',
+                            textOverflow: 'ellipsis',
+                            fontSize: '100%'
+                        }}>
+                            <Link
+                                href={`/detailsRecipes/${id}`}
+                                color="inherit"
+                                underline="hover"
+                            >
+                                {recipeTitle}
+                            </Link>
+                        </Typography>
+                    </Tooltip>
                     <Stack
                         direction="row"
                         justifyContent={'space-between'}
