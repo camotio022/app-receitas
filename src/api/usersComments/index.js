d6e42ecf40e64f14da93a5d7ec300d2e94e840e1
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-08-29 16:56:12 -0300

    obtendo os comentários

--- src/api/usersComments/index.js
+++ src/api/usersComments/index.js
@@ -1,8 +1,25 @@
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { collection, query, where, getDocs, getDoc } from 'firebase/firestore';
 import { db } from '../../../firebase.config';
 
 export const api_comments = {
     comments: {
+        get: async (id) => {
+            if (id) {
+                const docSnap = await getDoc(doc(db, "recipesComments", id));
+                if (docSnap.exists()) {
+                    return docSnap.data();
+                } else {
+                    console.log("No such document!");
+                }
+                return {};
+            }
+            const querySnapshot = await getDocs(collection(db, "recipesComments"));
+            const commentData = [];
+            querySnapshot.forEach((doc) => {
+                commentData.push(doc.data());
+            });
+            return commentData;
+        },
         getForRecipe: async (recipeId) => {
             if (!recipeId) return
             try {
