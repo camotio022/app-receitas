698bd0c0bf3751b0df62d1ba67fe6975b77b8948
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-11-24 15:41:11 -0300

    removendo os childrens nos links

--- src/pages/MainLayout/consts/links.jsx
+++ src/pages/MainLayout/consts/links.jsx
@@ -27,115 +27,27 @@ import {
   Favorite as FavoriteIcon,
 } from '@mui/icons-material'
 export const links = [
-  // {
-  //   icon: <HomeIcon />,
-  //   name: 'Home',
-  //   onClick: 'home',
-  //   children: [
-  //     {
-  //       icon: <HouseIcon />,
-  //       name: 'Página principal',
-  //       onClick: 'home',
-  //     },
-
-  //     { icon: <LastPageIcon />, name: 'Sub página', onClick: 'subPage' },
-  //     {
-  //       icon: <BookmarkIcon />,
-  //       name: 'Receitas favoritas',
-  //       onClick: 'favRecipes',
-  //     },
-  //     { icon: <EditNoteIcon />, name: 'Rascunho', onClick: 'draft' },
-  //   ],
-  // },
+  { name: 'Receitas', icon: <StarIcon />, link: '/topReview' },
   {
-    icon: <LayersIcon />,
-    name: 'Pages',
-    onClick: 'pages',
-    children: [
-      { name: 'Top Review', icon: <StarIcon />, link: '/topReview' },
-      // { name: 'Single Video', icon: <PlayCircleIcon /> },
-      // { name: 'Single Book', icon: <BookmarksIcon /> },
-      // { name: 'About us', icon: <InfoIcon /> },
-      // { name: 'Contacts', icon: <AlternateEmailIcon /> },
-    ],
+    name: 'Minhas Receitas',
+    icon: <BookIcon />,
+    link: '/my-recipes',
   },
   {
-    icon: <KitchenIcon />,
-    name: 'Receitas',
-    onClick: 'recipes',
-    children: [
-      {
-        name: 'Minhas Receitas',
-        icon: <BookIcon />,
-        link: '/my-recipes',
-      },
-      {
-        name: 'Receitas Favoritas',
-        icon: <FavoriteIcon />,
-        link: '/youfavoriteRecipes',
-      },
-
-      {
-        name: 'Create Recipe',
-        icon: <BookmarkAddIcon />,
-        link: '/createRecipes',
-      },
-      { name: 'Gerador de receitas', icon: <DinnerDiningIcon /> },
-    ],
+    name: 'Receitas Favoritas',
+    icon: <FavoriteIcon />,
+    link: '/youfavoriteRecipes',
   },
-  // {
-  //   icon: <BookIcon />,
-  //   name: 'Blog',
-  //   onClick: 'blog',
-  //   children: [
-  //     {
-  //       icon: <TextsmsIcon />,
-  //       name: 'Blog 1',
-  //       onClick: 'blog1',
-  //     },
 
-  //     {
-  //       icon: <MarkUnreadChatAltIcon />,
-  //       name: 'Blog 2',
-  //       onClick: 'blog2',
-  //     },
-  //     {
-  //       icon: <QuestionAnswerIcon />,
-  //       name: 'Blog 3 (Single)',
-  //       onClick: 'blog2',
-  //     },
-  //   ],
-  // },
   {
-    icon: <PeopleIcon />,
+    name: 'Nova Receita',
+    icon: <BookmarkAddIcon />,
+    link: '/createRecipes',
+  },
+  { name: 'Gerador de receitas', icon: <DinnerDiningIcon /> },
+  {
     name: 'Comunidade',
-    onClick: 'comunidade',
-    children: [
-      {
-        name: 'Comunidade',
-        icon: <Diversity3Icon />,
-        link: '/comunidade',
-        onClick: 'comunidade',
-      },
-      // { name: 'Grupos', icon: <RememberMeIcon /> },
-    ],
+    icon: <Diversity3Icon />,
+    link: '/comunidade',
   },
-  // {
-  //   icon: <SettingsIcon />,
-  //   name: 'Settings',
-  //   onClick: 'adSettings',
-  //   children: [
-  //     { icon: <TerminalIcon />, name: 'Mode root', onClick: 'root' },
-  //     {
-  //       icon: <AdminPanelSettingsIcon />,
-  //       name: 'Mode admin',
-  //       onClick: 'admin',
-  //     },
-  //     {
-  //       icon: <TaxiAlertIcon />,
-  //       name: 'Red alert mode ',
-  //       onClick: 'Red',
-  //     },
-  //   ],
-  // },
 ]
